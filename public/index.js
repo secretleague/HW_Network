2923d460ea0e1d8b41bc67b016ce3cb8e06ff3d9
Ivan Bunin <chernobelenkiy@gmail.com>
2022-05-05 23:16:53 +0300

    add requests

--- public/index.js
+++ public/index.js
@@ -1,11 +1,56 @@
-
 (function() {
-  let xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function() {
+  let $form, $result;
+
+  function getName() {
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
       if (xhr.readyState == XMLHttpRequest.DONE) {
-          console.log(xhr.responseText);
+        $result.textContent = xhr.responseText;
       }
+    }
+    xhr.open('GET', '/api/name', true);
+    xhr.send(null);
   }
-  xhr.open('GET', '/', true);
-  xhr.send(null);
+
+  function setName(data) {
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", '/api/name', true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+
+    xhr.onreadystatechange = function() {
+      if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+        $result.textContent = xhr.responseText;
+      }
+    }
+    xhr.send(JSON.stringify(data));
+  }
+
+  function prepareData() {
+    let data = {};
+    let formData = new FormData($form);
+    console.log(formData)
+
+    formData.forEach(function(value, key){
+      data[key] = value;
+    });
+
+    return data;
+  }
+  
+
+  document.addEventListener('DOMContentLoaded', () => {
+    $form = document.getElementById('profile');
+    $result = document.querySelector('.result');
+    getName();
+    
+
+    $form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      setName(prepareData());
+    });
+  });
 })();
+
+
+// замыкание
--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
-const public = path.join(__dirname, 'public');
+const public = path.join(__dirname, '/public');
+
+let data = {
+  name: 'Ivan'
+};
 
 const port = 3000;
 const host = 'localhost';
 
 app.use(express.static(public));
+
+const jsonParser = bodyParser.json();
 
 app.set('views', './views');
 app.set('view engine', 'pug');
@@ -14,6 +21,15 @@ app.set('view engine', 'pug');
 app.get('/', (req, res) => {
 	res.render('index');
 });
+
+app.get('/api/name', (req, res) => {
+	res.json(data);
+});
+
+app.post('/api/name', jsonParser, function (req, res) {
+  data = req.body;
+  res.json(data);
+})
 
 app.listen(port, host, () => {
 	console.log(`Server started at ${host} port ${port}`);
